8bba8479b1482705fc0949322c5f2fcae7ed0fe5
Panagiotis Ntenezos <Panagiotis.NTENEZOS@intrasoft-intl.com>
2021-05-11 00:19:53 +0300

    validateNumberSize refactor - makes use of lambda

--- src/main/java/core/service/ValidationService.java
+++ src/main/java/core/service/ValidationService.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 public class ValidationService {
     private static final Logger logger = LoggerFactory.getLogger(ValidationService.class);
@@ -20,24 +22,31 @@ public class ValidationService {
      * @return boolean
      */
     public boolean validateNumberSize(String[] input) {
-        for (String x : input) {
-            if (x.length() > 3 || !x.matches("\\d+")) {
-                return false;
-            }
+        Pattern pattern = Pattern.compile("\\d+");
+        if (input != null) {
+            return Arrays.stream(input).noneMatch(x -> x.length() > 3 || !pattern.matcher(x).matches());
         }
-        return true;
+        return false;
     }
 
+    /**
+     * Check that each possible number is a valid Greek telephone number
+     *
+     * @param phoneNumber a Set with all the possible numbers
+     * @return String
+     */
     public String isValidGreekPhoneNumber(Set<String> phoneNumber) {
-        List<String> result = new ArrayList<String>();;
-        phoneNumber.forEach(values -> {
+        List<String> result = new ArrayList<String>();
+        int counter = 0;
+        for (String values : phoneNumber){
+            counter++;
             if ((values.length() == 10 && (values.startsWith("2") || values.startsWith("69"))) ||
                     (values.length() == 14 && (values.startsWith("00302") || values.startsWith("003069")))) {
-                result.add(values + " - phone number: VALID");
+                result.add("Interpretation " + counter + ": " + values + " [phone number: VALID]");
             } else {
-                result.add(values + " - phone number: INVALID");
+                result.add("Interpretation " + counter + ": " + values + " [phone number: INVALID]");
             }
-        });
+        }
         return String.join("\n\n", result);
     }
 
