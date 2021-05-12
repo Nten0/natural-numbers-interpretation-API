0634438205eb5b922de3088f698a3b8cbf684653
Panagiotis Ntenezos <Panagiotis.NTENEZOS@intrasoft-intl.com>
2021-05-12 15:41:24 +0300

    possibleAmbiguitiesIdentifier improvements

--- src/main/java/core/service/PhoneNumberInterpretationService.java
+++ src/main/java/core/service/PhoneNumberInterpretationService.java
@@ -42,26 +42,40 @@ public class PhoneNumberInterpretationService {
                 } else if (input[0].endsWith(ZERO) && input[1].length() == 1) {// eg 830 5
                     possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 2, input.length), possibleAmbiguity + input[0] + input[1], possibleAmbiguities); // eg 8305
                     possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 2, input.length), possibleAmbiguity + input[0].substring(0, 2) + input[1], possibleAmbiguities); // eg 835
-//                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 2, input.length), possibleAmbiguity + input[0].charAt(0) + DOUBLE_ZERO + input[0].charAt(1) + input[1], possibleAmbiguities); // eg 80035
-//                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 2, input.length), possibleAmbiguity + input[0].charAt(0) + DOUBLE_ZERO + input[0].charAt(1) + ZERO + input[1], possibleAmbiguities); // eg 800305
+/**/                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 2, input.length), possibleAmbiguity + input[0].charAt(0) + DOUBLE_ZERO + input[0].charAt(1) + input[1], possibleAmbiguities); // eg 80035
+/**/                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 2, input.length), possibleAmbiguity + input[0].charAt(0) + DOUBLE_ZERO + input[0].charAt(1) + ZERO + input[1], possibleAmbiguities); // eg 800305
                 } else if (input[0].substring(0, 2).endsWith(ZERO)) { // eg 702 following by any 3-digit number
                     possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0], possibleAmbiguities); // eg 702
                     possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0].replace(ZERO, EMPTY_STRING), possibleAmbiguities); // eg 72
                 } else { // eg 835
-                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0], possibleAmbiguities); //eg 835
-//                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0].substring(0, 2) + ZERO + input[0].charAt(2), possibleAmbiguities); // eg 8305
-//                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0].charAt(0) + DOUBLE_ZERO + input[0].substring(1, 3), possibleAmbiguities); // eg 80035
-//                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0].charAt(0) + DOUBLE_ZERO + input[0].charAt(1) + ZERO + input[0].charAt(2), possibleAmbiguities); // eg 800305
+                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0], possibleAmbiguities); // eg 835
+/**/                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0].substring(0, 2) + ZERO + input[0].charAt(2), possibleAmbiguities); // eg 8305
+/**/                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0].charAt(0) + DOUBLE_ZERO + input[0].substring(1, 3), possibleAmbiguities); // eg 80035
+/**/                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0].charAt(0) + DOUBLE_ZERO + input[0].charAt(1) + ZERO + input[0].charAt(2), possibleAmbiguities); // eg 800305
                 }
             } else if (input[0].length() == 2) { // starts with a 2-digit number
                 if ((input[0].endsWith(ZERO) && !input[0].startsWith(ONE)) && input[1].length() == 1) { // eg 20 3
                     possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 2, input.length), possibleAmbiguity + input[0] + input[1], possibleAmbiguities); // eg 203
                     possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 2, input.length), possibleAmbiguity + input[0].charAt(0) + input[1], possibleAmbiguities); // eg 23
-                } else if (input[0].startsWith(ONE)) { // eg 10-19
-                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0], possibleAmbiguities); // eg 10-19
-                } else { // eg 23
-                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0], possibleAmbiguities); // eg 23
-                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0].charAt(0) + ZERO + input[0].charAt(1), possibleAmbiguities); // eg 20 3
+                } else if (!input[0].startsWith(ONE) && !input[0].endsWith(ZERO)) {
+                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0], possibleAmbiguities); // eg 93
+                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0].charAt(0) + ZERO + input[0].charAt(1), possibleAmbiguities); // eg 90 3
+                } else {
+                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0], possibleAmbiguities); // eg 13
+                }
+            }
+        } else if (input.length == 1 && input[0].length() > 1) { // the last number with 2 or 3 digits
+            if (input[0].length() == 3) { // eg 835
+                possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0], possibleAmbiguities);
+/**/                possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0].substring(0, 2) + ZERO + input[0].charAt(2), possibleAmbiguities); // eg 8305
+/**/                possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0].charAt(0) + DOUBLE_ZERO + input[0].substring(1, 3), possibleAmbiguities); // eg 80035
+/**/                possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0].charAt(0) + DOUBLE_ZERO + input[0].charAt(1) + ZERO + input[0].charAt(2), possibleAmbiguities); // eg 800305
+            } else if (input[0].length() == 2) { // eg 93 or 13
+                if (!input[0].startsWith(ONE) && !input[0].endsWith(ZERO)) {
+                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0], possibleAmbiguities); // eg 93
+                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0].charAt(0) + ZERO + input[0].charAt(1), possibleAmbiguities); // eg 90 3
+                } else {
+                    possibleAmbiguitiesIdentifier(ArrayUtils.subarray(input, 1, input.length), possibleAmbiguity + input[0], possibleAmbiguities); // eg 13
                 }
             }
         } else if (input.length > 0) { // 1 number eg 7 or 26 or 638
